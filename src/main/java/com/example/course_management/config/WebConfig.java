a23714c2584654e2c39929a309d718c134b2d425
danushanG <gdanushan0073@gmail.com>
2025-08-25 21:46:41 +0530

    Added webconfiguration

---
+++ src/main/java/com/example/course_management/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.example.course_management.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("course-management-frontend-production.up.railway.app") // frontend Railway URL
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
+}
+
